227496142acc654474881db19e433844fd071bcf
Luis Barreto <luis.barreto.marin@gmail.com>
2019-11-02 01:06:21 -0400

    Función crearConjunto
    -Implementación de la función crearConjunto
    -Cambios en los tipos de datos.

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include "conjuntos.h"
 
+void test();
+
 int main()
 {
-    printf("Hello world!\n");
+    test();
     return 0;
+}
+
+void test(){
+
+    ListaConjuntos clist = NULL;
+
+    char *universo[] = { "Hola","Mundo","Todo", "Bien" };
+
+    crearConjunto(clist,"A",universo,4);
+
 }
